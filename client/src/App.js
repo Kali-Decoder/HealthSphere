3e2418a8ce8f7ff76da9a2cea5108f281bc761fb
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2023-10-05 15:41:04 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import RegistrationPage from './pages/RegistrationPage';
 function App() {
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <>
+      <RegistrationPage/>
+    </>
   );
 }
 
--- server/Controllers/index.js
+++ server/Controllers/index.js
@@ -19,4 +19,20 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports={registerUser}
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ error: "User Not found" });
+    }
+    if (await bcrypt.compare(password, user.password)) {
+      if (res.status(201)) {
+        return res.json({ status: "ok", data: token });
+      } else {
+        return res.json({ error: "error" });
+      }
+    }
+    res.json({ status: "error", error: "Invalid Password" });
+  }
+
+module.exports={registerUser,loginUser}
